3640eb2d03588805950cdc8d83673f29e8c23eee
Alvaro <alvgonbar96@gmail.com>
2021-04-17 20:59:52 +0200

    Login empresa añadido

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,134 +1,117 @@
-//const { validationResult } = require('express-validator');
-
-//const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const connection = require('../database');
+require('dotenv').config()
 
-const Empleado = require('../models/empleado');
-/*
-exports.signup = async (req, res, next) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) return;
-
-  const name = req.body.name;
+exports.login = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 12);
+  const sqlEmpleado = 'SELECT * FROM empleados WHERE email = ?';
+  const sqlDirectivo = 'SELECT * FROM directivos WHERE email = ?';
+  const sqlEmpresa = 'SELECT * FROM empresa WHERE email = ?';
 
-    const userDetails = {
-      name: name,
-      email: email,
-      password: hashedPassword,
-    };
-
-    const result = await User.save(userDetails);
-
-    res.status(201).json({ message: 'User registered!' });
-  } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
+  connection.query(sqlEmpleado, [email], async (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err
+      });
     }
-    next(err);
-  }
-};*/
 
-exports.login = async (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  
-  try {
-    //const empleado = await Empleado.find(email);
-    const sqlEmpleado = 'SELECT * FROM empleados WHERE email = ?';
-    const sqlDirectivo = 'SELECT * FROM directivos WHERE email = ?';
-
-    connection.query(sqlEmpleado, [email], async(err, results, fields) => {
-      if (err) {
-        return res.status(500).json({
-            ok: false,
-            err
-        });
-    }
-    
     if (results.length == 0) {
-      //pruebo si es directivo
-      connection.query(sqlDirectivo, [email], async(err1, results1, fields) => {
+      connection.query(sqlDirectivo, [email], async (err1, results1) => {
         if (err1) {
           return res.status(500).json({
+            ok: false,
+            err1
+          });
+        }
+
+        if (results1.length == 0) {
+          /*return res.status(401).json({
+            ok: false,
+            message: 'Email o contraseña incorrectos'
+          });*/
+          connection.query(sqlEmpresa, [email], (err1, results1) => {
+            if (err1) {
+              return res.status(500).json({
+                ok: false,
+                err1
+              });
+            }
+            const empresa = results1[0];
+            console.log(empresa);
+            let tipo = 'empresa';
+            console.log(tipo);
+
+            //const isEqual = await bcrypt.compare(password, storedUser.password);
+            const isEqual = (password == empresa.password);
+
+            if (!isEqual) {
+              return res.status(401).json({
+                ok: false,
+                message: 'Email o contraseña incorrectos'
+              });
+            }
+
+            const token = jwt.sign(
+              {
+                email: empresa.email,
+                empresaId: empresa.idempresa,
+              },
+              process.env.JWT_KEY,
+              { expiresIn: process.env.JWT_EXPIRES_IN }
+            );
+            res.status(200).json({ token: token, empresaId: empresa.idempresa, tipo });
+          });
+        } else {
+          const storedUser = results1[0];
+          console.log("Directivo: ", storedUser);
+          let tipo = 'directivo';
+          console.log(tipo);
+
+          const isEqual = await bcrypt.compare(password, storedUser.password);
+
+          if (!isEqual) {
+            return res.status(401).json({
               ok: false,
-              err1
-          });
-      }
+              message: 'Email o contraseña incorrectos'
+            });
+          }
 
-        if (!results1[0]) {
-          return res.status(401).json({
-            ok: false,
-            message: 'Usuario o contraseña incorrectos'
-        });
+          const token = jwt.sign(
+            {
+              email: storedUser.email,
+              userId: storedUser.iddirectivo,
+            },
+            process.env.JWT_KEY,
+            { expiresIn: process.env.JWT_EXPIRES_IN }
+          );
+          res.status(200).json({ token: token, userId: storedUser.iddirectivo, tipo });
         }
-
-        const storedUser = results1[0];
-        console.log("Directivo: ", storedUser);
-        let tipo = 'directivo';
-        console.log(tipo);
-
-        const isEqual = storedUser.password == password;
-
-        if (!isEqual) {
-          //const error = new Error('Wrong password!');
-          //error.statusCode = 401;
-          //throw error;
-          return res.status(401).json({
-            ok: false,
-            message: 'Usuario o contraseña incorrectos'
-          });
-        }
-
-        const token = jwt.sign(
-          {
-            email: storedUser.email,
-            userId: storedUser.iddirectivo,
-          },
-          'secretfortoken',
-          { expiresIn: '1h' }
-        );
-        res.status(200).json({ token: token, userId: storedUser.iddirectivo, tipo });
-
       });
     } else {
-    const storedUser = results[0];
-    let tipo = 'empleado';
+      const storedUser = results[0];
+      let tipo = 'empleado';
 
-    //const isEqual = await bcrypt.compare(password, storedUser.password);
-    const isEqual = storedUser.password == password;
+      const isEqual = await bcrypt.compare(password, storedUser.password);
 
-    if (!isEqual) {
-      /*const error = new Error('Wrong password!');
-      error.statusCode = 401;
-      throw error;*/
-      return res.status(401).json({
-        ok: false,
-        message: 'Usuario o contraseña incorrectos'
-    });
+      if (!isEqual) {
+        return res.status(401).json({
+          ok: false,
+          message: 'Email o contraseña incorrectos'
+        });
+      }
+      const token = jwt.sign(
+        {
+          email: storedUser.email,
+          userId: storedUser.idempleado,
+        },
+        process.env.JWT_KEY,
+        { expiresIn: process.env.JWT_EXPIRES_IN }
+      );
+      res.status(200).json({ token: token, userId: storedUser.idempleado, tipo });
     }
-
-    const token = jwt.sign(
-      {
-        email: storedUser.email,
-        userId: storedUser.idempleado,
-      },
-      'secretfortoken',
-      { expiresIn: '1h' }
-    );
-    res.status(200).json({ token: token, userId: storedUser.idempleado, tipo });
-  }
-}); 
-  } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  }
+  });
 };
