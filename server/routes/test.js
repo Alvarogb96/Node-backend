c0c1a8530fc3feecadaecb99da217daf50d430df
Alvaro <alvgonbar96@gmail.com>
2021-06-09 21:12:17 +0200

    Consulta tests a base de datos

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,78 +1,18 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
+const router = express.Router();
+router.use(express.json());
+const testController = require('../controllers/testController');
 
-const connection = require('../database');
+//Todos los noticias
+router.get('/tests', testController.tests);
 
-app.get('/tests', (req, res) =>{
-    const sql = 'SELECT * FROM tests'
-    connection.query(sql, (err,results)=>{
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        } else if(results.length > 0){
-            res.json(results);
-            //res.json({empleado: results[0], ok:true});
-        } else {
-            res.send("No hay test");
-        }
-    });
-});
+//Añadir test
+router.post('/addTest', testController.addTest);
 
-app.get('/test/:idtest', (req, res) =>{
-    const { idtest } = req.params;
-    const sql = 'SELECT * FROM tests WHERE idtest = ?'
-    connection.query(sql, [idtest], (err,results)=>{
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        } else if(results.length > 0){
-            res.json(results);
-            //res.json({empleado: results[0], ok:true});
-        } else {
-            res.send("No hay test con ese id");
-        }
-    });
-});
+//Obtener test por id
+router.get('/tests/:id', testController.readTest);
 
-app.get('/tests/:idempleado', (req, res) =>{
-    const { idempleado } = req.params;
-    const sql = 'SELECT * FROM tests WHERE idempleado = ?'
-    connection.query(sql, [idempleado], (err,results)=>{
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        } else if(results.length > 0){
-            res.json(results);
-            //res.json({empleado: results[0], ok:true});
-        } else {
-            res.status(200).json({
-                ok: false,
-                message: 'El empleado no tiene test'
-              });
-        }
-    });
-});
+//Obtener test por id de usuario
+router.get('/testsEmpleado/:id', testController.readTestEmpleado);
 
-app.post('/addTest', (req, res) =>{
-    const sql = 'INSERT INTO tests SET ?';
-
-    const testObJ = {
-        idempleado: req.body.idempleado,
-        tipo: req.body.tipo,
-        resultado: req.body.resultado,
-        fecha: req.body.fecha,
-    };
-    connection.query(sql, testObJ, error => {
-        if (error) throw error;
-        res.send('test añadido');
-      });
-});
-
-module.exports = app;
+module.exports = router;
