24ce428dd943799eff79e2f6f735fd4c2cf603a7
Alvaro <alvgonbar96@gmail.com>
2021-06-10 18:39:02 +0200

    Cambios usuarioController

--- server/controllers/usuarioController.js
+++ server/controllers/usuarioController.js
@@ -3,28 +3,19 @@ const bcrypt = require('bcryptjs');
 
 exports.addEmpleado = async(req, res) => {
     const sql = 'INSERT INTO usuarios SET ?';
-    const password = req.body.password;
+    const password = req.body.usuario.password;
     const hashedPassword = await bcrypt.hash(password, 12);
-    const empleadoObJ = {
-        nombre: req.body.nombre,
-        apellidos: req.body.apellidos,
-        nif: req.body.nif,
-        fecha_nacimiento: req.body.fecha_nacimiento,
-        fecha_incorporacion: req.body.fecha_incorporacion,
-        role: 'E',
-        email: req.body.email,
-        password: hashedPassword,
-        
-    };
-    connection.query(sql, empleadoObJ, error => {
+    req.body.usuario.password = hashedPassword;
+    const usuario = req.body.usuario;
+    connection.query(sql, usuario, error => {
         if (error) throw error;
-        res.send('Empleado añadido');
+        res.send('Usuario añadido');
       });
 }
 
 exports.readEmpleado = (req, res) =>{
     const { id } = req.params;
-    const sql = 'SELECT * FROM usuarios WHERE id_usuario =? AND role="emp"';
+    const sql = 'SELECT * FROM usuarios WHERE id_usuario =? AND role="'+process.env.ROLE_EMPLEADO+'"';
     connection.query(sql, [id] ,  (err,results)=>{
         if (err) {
             return res.status(400).json({
