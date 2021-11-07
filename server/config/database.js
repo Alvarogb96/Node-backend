116bdef0ad7a6a6c80e26cdc3dc8f6f12125c241
Alvaro <alvgonbar96@gmail.com>
2021-11-07 16:13:20 +0100

    Conexión BD

--- server/config/database.js
+++ server/config/database.js
@@ -1,20 +1,20 @@
 var mysql      = require('mysql');
 //Local
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'root',
-//   database : 'tfg_database'
-// });
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'tfg_database'
+});
 
 //Prod
-var connection = mysql.createConnection({
-    host     : 'tfg.cxx0lf4ek1mi.us-east-2.rds.amazonaws.com',
-    user     : 'root',
-    password : 'rootroot',
-    database : 'tfg_database',
-    port: 3306
-  });
+// var connection = mysql.createConnection({
+//     host     : 'tfg.cxx0lf4ek1mi.us-east-2.rds.amazonaws.com',
+//     user     : 'root',
+//     password : 'rootroot',
+//     database : 'tfg_database',
+//     port: 3306
+//   });
  
 connection.connect(function (err){
     if(err){
